6a65073ee7ccd4dbde1246fab946baca269871ca
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-06-18 18:00:10 +0900

    XR_Defence Gun Reload (Soket & Reload)
    XR_Defence Gun Reload (Soket & Reload)
    (소켓 부착시 리로드 기능)

---
+++ XR_Defende/Assets/Scripts/Weapon/Gun/WeaponStand.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class WeaponStand : MonoBehaviour
+{
+   
+    public void OnSoketed(SelectEnterEventArgs args)
+    {
+        var reloadable = args.interactableObject.transform.GetComponent<IReloadable>();
+        reloadable?.StartReload();
+    }
+
+    public void OnUnsoketed(SelectExitEventArgs args) 
+    {
+        var reloadable = args.interactableObject.transform.GetComponent<IReloadable>();
+        reloadable?.StopReload();
+    }
+}
